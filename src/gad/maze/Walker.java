c5ee191f4d004a925ae8ca802bdea0c50b86116a
ge94jip <ge94jip@mytum.de>
2023-04-27 17:38:12 +0200

    bb

--- src/gad/maze/Walker.java
+++ src/gad/maze/Walker.java
@@ -14,6 +14,9 @@ public class Walker {
 	int ry = 0;
 	int fx = 0;
 	int fy = 0;
+	int x = 1;
+	int y = 0;
+	int dir = 0;
 
 
 	public boolean walk() {
@@ -24,12 +27,58 @@ public class Walker {
 		else{
 			return false;
 		}
-
-
-		return alg(1,1, 0);
+		while ((x != maze.length - 1) && (y != maze[0].length - 2) || (x != 1) && (y != 0) ) {
+			switch (dir % 4) {
+				case 0:
+					rx = x - 1;
+					ry = y;
+					fx = x;
+					fy = y + 1;
+					break;
+				case 1:
+					rx = x;
+					ry = y + 1;
+					fx = x + 1;
+					fy = y;
+					break;
+				case 2:
+					rx = x + 1;
+					ry = y;
+					fx = x;
+					fy = y - 1;
+					break;
+				case 3:
+					rx = x;
+					ry = y - 1;
+					fx = x - 1;
+					fy = y;
+					break;
+			}
+			if (!maze[rx][ry]) {
+				result.addLocation(rx, ry);
+				x = rx;
+				y = ry;
+				dir--;
+			} else {
+				if (!maze[fx][fy]) {
+					result.addLocation(fx, fy);
+					x = fx;
+					y = fy;
+				} else {
+					dir++;
+				}
+			}
+		}
+		if(x == 0){
+			return false;
+		}
+		else {
+			return true;
+		}
+		//return alg(1,1, 0);
 	}
 
-	public boolean alg (int a, int b, int dir){
+	/*public boolean alg (int a, int b, int dir){
 		if (dir < 0 ){
 			dir = dir + 4;
 		}
@@ -78,13 +127,13 @@ public class Walker {
 			}
 		}
 
-	}
+	}*/
 
 	public static void main(String[] args) {
-		boolean[][] maze = Maze.generateStandardMaze(999, 999);
+		boolean[][] maze = Maze.generateStandardMaze(9999999, 100);
 		StudentResult result = new StudentResult();
 		Walker walker = new Walker(maze, result);
 		System.out.println(walker.walk());
-		Maze.draw(maze, result);
+
 	}
 }
